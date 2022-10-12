28f3b523efedc526d8c6410843af4853eb08b8ed
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-10-12 09:29:32 +0200

    Initial Commit

---
+++ MagicEightBall Watch App/MagicEightBallApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicEightBallApp.swift
+//  MagicEightBall Watch App
+//
+//  Created by Zaid Neurothrone on 2022-10-12.
+//
+
+import SwiftUI
+
+@main
+struct MagicEightBall_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
